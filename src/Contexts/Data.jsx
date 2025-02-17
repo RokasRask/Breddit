935b73ab5645727f6d10507fdc4ed49940af0b23
RokRask <rokas.raskevic@gmail.com>
2025-02-17 13:15:18 +0200

    commit

--- src/Contexts/Data.jsx
+++ src/Contexts/Data.jsx
@@ -1,15 +1,15 @@
 import { createContext } from 'react';
 import usePosts from '../Hooks/usePosts';
- 
+
 const DataContext = createContext();
- 
+
 export const Data = ({ children }) => {
 
     const { posts } = usePosts();
- 
+
     return (
         <DataContext.Provider value={{
- 
+            posts
         }}>
             {children}
         </DataContext.Provider>
