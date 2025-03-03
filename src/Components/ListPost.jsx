5079cf68ef49d334ddb5c58600a64239cd2943af
RokRask <rokas.raskevic@gmail.com>
2025-03-03 13:49:45 +0200

    commit

--- src/Components/ListPost.jsx
+++ src/Components/ListPost.jsx
@@ -5,9 +5,10 @@ import AuthContext from '../Contexts/Auth';
 import * as C from '../Constants/actions';
 import useVote from '../Hooks/useVote';
 
+
 export default function ListPost({ post }) {
 
-    const { comments, getComments, dispachPosts, addPostNewComment } = useContext(DataContext);
+    const { comments, getComments, dispachPosts, addNewPostComment } = useContext(DataContext);
 
     const { user } = useContext(AuthContext);
 
@@ -18,7 +19,8 @@ export default function ListPost({ post }) {
     const vote = useRef(false);
 
     const addPostComment = _ => {
-        addPostNewComment(post.id, postCom, user.name);
+        console.log('add');
+        addNewPostComment(post.id, postCom, user);
         setPostCom('');
     }
 
@@ -31,7 +33,7 @@ export default function ListPost({ post }) {
 
         setLikes(post.likes);
 
-        console.log('pakito', post.id)
+        console.log('pakito', post.id);
 
     }, [post])
 
@@ -74,20 +76,23 @@ export default function ListPost({ post }) {
                 <span className="comment" onClick={_ => getComments(post.id, 'post')}>Show all comments: {post.comments}</span>
             </div>
             <div className="post-comments">
-                <h3>Comments</h3>
+
 
                 {
                     comments
                         .filter(comment => comment.postId === post.id && comment.comId === null)
                         .map(comment => <ListComment key={comment.id} comment={comment} />)
                 }
+
                 {
                     user.role !== 'guest' &&
-                    <div className='write-comment'>
+
+                    <div className="write-comment">
                         <div>Write comment</div>
                         <textarea onChange={e => setPostCom(e.target.value)} value={postCom} />
-                        <button className='blue' onClick={addPostComment}>send</button>
+                        <button className="blue" onClick={addPostComment}>send</button>
                     </div>
+
                 }
 
             </div>
--- src/Hooks/useComments.jsx
+++ src/Hooks/useComments.jsx
@@ -8,52 +8,53 @@ export default function useComments() {
     const [comments, setComments] = useState([]);
     /* 
     [
-        {id: 1, postId: 1, comId: null, body: 'comment body', author: 'author name'},
-        {id: 2, postId: 1, comId: 2, body: 'comment body', author: 'author name'},
-        {id: 3, postId: 1, comId: 3, body: 'comment body', author: 'author name'},
-
+        {id: 1, postId: 1, comId: null, body: 'comment body', author: 'author name', likes:{l:[],d:[]}},
+        {id: 2, postId: 1, comId: 2, body: 'comment body', author: 'author name', likes:{l:[],d:[]}},
+        {id: 3, postId: 1, comId: 3, body: 'comment body', author: 'author name', likes:{l:[],d:[]}},
     ];
     */
 
-    const addPostNewComment = async (postId, commentText, user) => {
+    const addNewPostComment = async (postId, commentText, user) => {
         setComments(c => [...c, {
             id: uuidv4(),
             postId,
             comId: null,
             body: commentText,
             author: user.name,
-            likes: { l: [], d: [] }
+            likes:{l:[],d:[]}
         }]);
         try {
             await axios.post(serverUrl + 'create-comment/' + postId + '/post', {
                 author_id: user.id,
                 content: commentText
             });
+
+        }catch (error) {
+            console.error(error);
+        }
+    }
+
+
+    const getComments = async (id, type) => {
+        try {
+            const response = await axios.get(serverUrl + 'comments/' + id + '/' + type);
+            console.log('Ateina atsakymas iš serverio į useComments:', response.data);
+            setComments(comments => {
+                const c = structuredClone(comments);
+                response.data.forEach(res => {
+                   const copy = c.find(c => c.id === res.id);
+                   if (copy) {
+                       copy.body = res.body;
+                   } else {
+                       c.push(res);
+                   }
+                });
+                return c;
+            });
         } catch (error) {
             console.error(error);
         }
-
-
-        const getComments = async (id, type) => {
-            try {
-                const response = await axios.get(serverUrl + 'comments/' + id + '/' + type);
-                console.log('Ateina atsakymas iš serverio į useComments:', response.data);
-                setComments(comments => {
-                    const c = structuredClone(comments);
-                    response.data.forEach(res => {
-                        const copy = c.find(c => c.id === res.id);
-                        if (copy) {
-                            copy.body = res.body;
-                        } else {
-                            c.push(res);
-                        }
-                    });
-                    return c;
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        }
-
-        return { comments, getComments, addPostNewComment };
     }
+      
+    return { comments, getComments, addNewPostComment };
+}
